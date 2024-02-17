b21d4859c8b3d91a9c489350aaf65758e988095e
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2024-02-17 19:51:00 +0530

    🔨 Update aliases.sh

--- scripts/shell/aliases.sh
+++ scripts/shell/aliases.sh
@@ -2,7 +2,7 @@
 
 # Custom Alias of @arv-anshul
 alias cls="clear"
-alias Codes="cd ~/Developer/Codes-Local"
+alias dev="cd ~/Developer"
 alias dk="docker"
 alias dkc="docker-compose"
 alias dmake="make -f ~/Developer/Makefile"
@@ -13,29 +13,29 @@ alias zz="clear"
 
 # Alias for GIT
 alias g="git"
-alias ga="git add";
+alias ga="git add"
 alias gac="git add . && git commit -a -m"
-alias gb="git branch";
-alias gch="git checkout";
-alias gcm="git checkout main";
-alias gd="git diff";
-alias gf="git fetch origin";
-alias gfa="git fetch --all";
+alias gb="git branch"
+alias gch="git checkout"
+alias gcm="git checkout main"
+alias gd="git diff"
+alias gf="git fetch origin"
+alias gfa="git fetch --all"
 alias glog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches"
-alias gpull="git pull";
-alias gpush="git push";
+alias gpull="git pull"
+alias gpush="git push"
 alias gra="git remote add"
-alias grf="git reflog";
+alias grf="git reflog"
 alias grv="git remote -v"
-alias gs="git status";
-function gc { git commit -m "$@"; }
+alias gs="git status"
+alias gc="git commit -m"
 
 # Alias for Python
 alias py="python3"
 
 # Alias for rye
 alias pyls="rye toolchain list"
-alias rsyn="rye sync --no-dev --no-lock"
+alias rsyn="rye sync --no-dev"
 alias ryn="rye run"
 
 function pycls() {
